0fe6dd3fe9867754187bfe64e966736741daa9fb
Jacob Sierra <jacobisaac22@yahoo.com>
2021-05-22 22:03:18 -0500

    updated css and added functions to show next question

--- css/script.js
+++ css/script.js
@@ -4,24 +4,45 @@ var answerBtn1 = document.querySelector(".answer1");
 var answerBtn2 = document.querySelector(".answer2");
 var answerBtn3 = document.querySelector(".answer3");
 var answerBtn4 = document.querySelector(".answer4");
-var trueBtn = document.querySelector(".answerTrue");
-var falseBtn = document.querySelector(".answerFalse");
-var countdownTimer = 30;
+var countdownTimer = 60;
 var timer = document.querySelector(".timer")
 var score = 20;
-var question1 = [
-    {question: " what is the first number place holder of an Array  ",
+var questions = [
+    {question: " What is the first number place holder of an Array? ",
      answerBtn1: "1",
      answerBtn2: "2",
      answerBtn3: "0",
      answerBtn4: "3",
-     correct: "0"}
-]
-var questionBox = document.querySelector(".question-1")
+     correct: "3"},
+    { question: " What is Boolean defined as ?",
+      answerBtn1: "Yes/No",
+      answerBtn2: "True/False",
+      answerBtn3: "Maybe",
+      answerBtn4: "All of the above",
+      correct: "2"},
+    { question: "What is the HTML tag under which one can write the JavaScript code?",
+      answerBtn1: "<javascript>",
+      answerBtn2: "<scripted>",
+      answerBtn3: "<js>",
+      answerBtn4: "<script>",
+      correct: "4"},
+    { question: " Which of the following is not a data type? ",
+      answerBtn1: "Boolean",
+      answerBtn2: "String",
+      answerBtn3: "Variable",
+      answerBtn4: "Number",
+      correct: "3"},
+    { question: " What logs data directly to the console?",
+      answerBtn1: "console.log",
+      answerBtn2: "console.set",
+      answerBtn3: "console.save",
+      answerBtn4: "console.input",
+      correct: "1"},
+];
+
+var questionBox = document.getElementById("question-1")
 var questionIndex = 0;
-
-
-
+var nextQuestion = questions.length - 1;
 
 
 
@@ -30,30 +51,46 @@ var questionIndex = 0;
 startBtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
-    var timerInterval = setInterval(function (){
+// added function to run first question display
+startBtn.classList.add("hide");
+instructions = document.querySelector(".instructions");
+instructions.classList.add("hide");
+title = document.querySelector(".title");
+title.classList.add("hide");
+answerBtn1.classList.remove("hide");
+answerBtn2.classList.remove("hide");
+answerBtn3.classList.remove("hide");
+answerBtn4.classList.remove("hide");
+displayQuestion();
+
+
+var timerInterval = setInterval(function (){
      countdownTimer--;
      timer.textContent = countdownTimer + " seconds left";
-     firstQuestion()
      
-     if (countdownTimer === 0) {
+     
+     if (countdownTimer  === 0 || questionIndex < 4) {
          clearInterval(timerInterval);
          return;
      }
- }, 1000);
-};
-
-// added function to run first question display
-function firstQuestion(){
-     q = question1[questionIndex];
-     questionBox.innerHTML = q.question ;
-     answerBtn1.innerHTML = q.answerBtn1;
-     answerBtn2.innerHTML = q.answerBtn2;
-     answerBtn3.innerHTML = q.answerBtn3;
-     answerBtn4.innerHTML = q.answerBtn4;
-
+ }, 1000)
 }
-
-function firstAnswer(){
- if (question1[questionIndex].correct == answer);
- console.log("yes")
+ 
+function displayQuestion() {
+   quest = questions[questionIndex];
+   questionBox.textContent = quest.question;
+   answerBtn1.textContent = quest.answerBtn1;
+   answerBtn2.textContent = quest.answerBtn2;
+   answerBtn3.textContent = quest.answerBtn3;
+   answerBtn4.textContent = quest.answerBtn4;
+};
+//added function to run next question 
+function questionAnswer(answer){
+    if(questions[questionIndex].correct == answer){
+        console.log("correct")
+    } else {
+        console.log("wrong")
+    }
+    questionIndex++
+    displayQuestion()
 }
